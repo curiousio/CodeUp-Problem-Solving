9eff72fe015adbb222f09201feb94d70d5d74eb6
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-12 12:50:26 +0900

    Create 1260.c
    3의 배수의 합

---
+++ 1260.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, i, sum = 0;
+
+	scanf("%d %d", &a, &b);
+
+	for (i = a; i <= b; i++)	
+	{
+		if (i % 3 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d", sum);
+
+	return 0;
+}
