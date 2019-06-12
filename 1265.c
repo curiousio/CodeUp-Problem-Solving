0dcac63416259f447b66ab388056a758e0384c74
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-12 13:00:31 +0900

    Create 1265.c
    구구단 출력하기 1

---
+++ 1265.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= 9; i++)
+    {
+        printf("%d*%d=%d\n", n, i, n * i);
+    }
+
+    return 0;
+}
