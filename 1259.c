fa4260c7cc52037f222e65b7d76d69fffe3ded5c
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-12 12:49:22 +0900

    Create 1259.c
    1부터 n까지 중 짝수의 합 구하기

---
+++ 1259.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, i, sum = 0;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)	
+	{
+		if (i % 2 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d", sum);
+
+	return 0;
+}
