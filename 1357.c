695f1cf29bbc0d3eab142fc977a3a16df4a0c384
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-15 23:48:27 +0900

    Create 1357.c
    삼각형 출력하기 4

---
+++ 1357.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n * 2 - 1; i++)
+    {
+        if (i <= n)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+        }
+        else
+        {
+            for (j = 1; j <= n * 2 - i; j++)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
