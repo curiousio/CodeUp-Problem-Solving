73b2f0d41711a629b6669655fa57ebb6fe911393
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-19 01:00:31 +0900

    Create 1361.c
    별 계단 만들기

---
+++ 1361.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j <= i + 2; j++)
+        {
+            if (i >= j)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
