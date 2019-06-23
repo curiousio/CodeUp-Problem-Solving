b5a58ff97a495fef9bd106ba14a071a5bd35e4c4
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-24 00:14:41 +0900

    Create 1541.c
    [기초-함수작성] 함수로 negative/zero/positive 출력하기

---
+++ 1541.c
@@ -0,0 +1,17 @@
+void f(int n)
+{
+    if (n == 0)
+    {
+        printf("zero");
+    }
+    else if (n < 0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("positive");
+    }
+
+    return;
+}
