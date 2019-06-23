498a2c61ef9c443bc61c9e33d5d4676e19914b73
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-24 00:04:01 +0900

    Create 1537.c
    [기초-함수작성] 함수로 hello 또는 world 출력하기

---
+++ 1537.c
@@ -0,0 +1,17 @@
+void f(int n)
+{
+    if (n == 1)
+    {
+        printf("hello\n");
+    }
+    else if (n == 2)
+    {
+        printf("world\n");
+    }
+    else
+    {
+        printf("\n");
+    }
+
+    return;
+}
