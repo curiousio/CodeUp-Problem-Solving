8f19028b8421bc7dace61de2d6c021f59b73225e
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-06-27 00:32:13 +0900

    Create 1463.c
    [기초-배열연습] 2차원 배열 순서대로 채우기 1-4

---
+++ 1463.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, k, data[100][100];
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        k = n - i;
+
+        for (j = 0; j < n; j++)
+        {
+            data[i][j] = k += n;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", data[i][j] - n);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
