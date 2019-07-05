d9320753a9d118472257dad001f98b2c16bb9a10
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-07-05 13:24:20 +0900

    Create 1502.c
    2차원 배열 채우기 2

---
+++ 1502.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, count = 1;
+
+    scanf("%d", &n);
+
+    int data[n][n];
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            data[j][i] = count++;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", data[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
