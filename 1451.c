2560c80d3c4db029bb6209abd6bcd5c81f8467dc
samuelkim413 <46525418+samuelkim413@users.noreply.github.com>
2019-07-31 13:47:22 +0900

    Create 1451.c
    데이터 정렬 (small)

---
+++ 1451.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, arr[10000], temp;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
