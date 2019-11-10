b4919dbe210cf8886e47d67aa366b1a07abb467e
miniesam <46525418+miniesam@users.noreply.github.com>
2019-11-10 12:53:18 +0900

    Create 1070.c
    [기초-조건/선택실행구조] 월 입력받아 계절 출력하기(설명)

---
+++ 1070.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int month;
+
+    scanf("%d", &month);
+
+    switch (month)
+    {
+        case 3:
+        case 4:
+        case 5:
+            printf("spring");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            printf("summer");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            printf("fall");
+            break;
+        case 12:
+        case 1:
+        case 2:
+            printf("winter");
+            break;
+    }
+
+    return 0;
+}
